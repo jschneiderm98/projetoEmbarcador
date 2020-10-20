3450d87c1ca16a3686fa8a0d251fe74d136b24bd
jschneiderm98 <julio_schneider@hotmail.com>
2020-10-20 19:30:42 -0300

    Adiçao da funçao de medir temperatura
    Co-authored-by: arivanen <arivane.machado@gmail.com>

---
+++ src/components/tempMeasure.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float tempMeasure(){
+    system("sudo modprobe w1-gpio");
+    system("sudo modprobe w1-therm");
+    int i=0;
+    float temp;
+    FILE  *fp;
+    char leitura[100];
+    while(i<10)
+    {
+    system("cat /sys/bus/w1/devices/28-3c01a81664b5/w1_slave > temperature.txt");
+    fp=fopen("temperature.txt","r");
+    if(fp == NULL) return -1;
+    fscanf(fp, "%*[^\n]\n");
+    fscanf(fp,"%*29c%s",leitura);
+    temp = strtof(leitura, NULL);
+    temp /= 1000;
+    temp = temp - 5;
+    fprintf(stderr, "temperatura :%f\n", temp);
+    
+    fclose(fp);
+    i++;
+    }
+	return temp;
+}
+
